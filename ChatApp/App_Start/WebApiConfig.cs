64385c389f2a80674b7220b968558dde0ec41b76
YourName <noor11809027@gmail.com>
2022-10-28 19:48:37 +0600

    cors add

--- ChatApp/App_Start/WebApiConfig.cs
+++ ChatApp/App_Start/WebApiConfig.cs
@@ -10,7 +10,8 @@ namespace ChatApp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            //New Code
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
